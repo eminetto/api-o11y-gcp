efefd0ede62daf4865a048428a3d5cbaa71c8176
Elton Minetto <elton@minetto.dev>
2024-03-12 08:38:19 -0300

    fix: add rollback

--- vote/sql/sql.go
+++ vote/sql/sql.go
@@ -56,6 +56,7 @@ func (r *SQL) Store(ctx context.Context, v *vote.Vote) error {
 	if err != nil {
 		span.RecordError(err)
 		span.SetStatus(codes.Error, err.Error())
+		tx.Rollback()
 		return err
 	}
 	return nil
